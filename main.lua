a32796516a77d13b5dc55fb02f66e35526ef5254
Nitroxyz <vladhust@gmail.com>
2025-01-23 15:13:08 +0100

    Version 1.9.0

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 meta = {
     name = "ExtraEyes",
-    version = "1.8.3",
+    version = "1.9.0",
     author = "Nitroxy",
     description = "Shows held items",
     online_safe = true
 }
 --Versions after 5.3:
---8
+--9
 --[[ TODO: 
 ADD DISPLAY FOR HIRED HAND
 Put the icon to the right of the ropes pls
@@ -36,8 +36,6 @@ local textures = {}
 local columns = {}
 local rows = {}
 
-local sources = {}
-
 -- All use the same
 local backitem_texture = 373
 
@@ -55,14 +53,6 @@ set_callback(function()
 
     local new_state = get_local_state() --[[@as StateMemory]]
 
-    --[[
-    if new_state.screen ~= state.screen then
-        --print("Screen mix")
-    end 
-    if new_state.loading ~= state.loading then
-        --print("Loading mix")
-    end 
-    ]]
     if new_state.screen ~= SCREEN.LEVEL or new_state.loading == 0 then
         for slot = 1, 4 do
             local player = get_player(slot, false)
@@ -71,15 +61,16 @@ set_callback(function()
                 local held_item = player:get_held_entity()
                 if held_item then
                     local texture = held_item:get_texture()
-                    if not texture then
+                    --if not texture then
                         --print("THIS NEEDS TO BE INVESTIGATED")
                         --prinspect(held_item.type.id)
-                    end
+                    --end
                     local texture_width = math.floor(get_texture_definition(texture).width / get_texture_definition(texture).tile_width)
                     textures[slot] = texture;
                     columns[slot] = held_item.animation_frame % texture_width;
                     rows[slot] =  math.floor(held_item.animation_frame / texture_width);
-
+                    
+                    --[[
                     local redner = held_item.rendering_info
                     --sources[slot] = redner.source
 
@@ -92,17 +83,18 @@ set_callback(function()
                         --print("Texture num is higher!")
                         --prinspect(redner.texture_num)
                     end
+                    ]]
                 end
 
                 -- Back items!!!
                 local backi = player:worn_backitem()
-                player_backitem[slot] = backitem[get_entity_type(backi)]
-                --prinspect(backitem[get_entity_type(backi)])
-                --prinspect(player_backitem)
+                if backi ~= -1 then
+                    player_backitem[slot] = backitem[get_entity_type(backi)]
+                end
             end
         end
     end
-end, ON.POST_UPDATE)
+end, ON.POST_GAME_LOOP)
 
 set_callback(function(render_ctx, hud)
     render_ctx = render_ctx --[[@as VanillaRenderContext]]
@@ -131,16 +123,14 @@ set_callback(function(render_ctx, hud)
             local temp_aabb = AABB:new(x, y, x + b, y - (b * (16/9)));
             local temp_quad = Quad:new(temp_aabb);
 
+            --[[
             local temp_aabb2 = AABB:new(x, y-0.1, x + b, y - (b * (16/9)));
             local temp_quad2 = Quad:new(temp_aabb2);
+            ]]
 
             if textures[slot] then
                 render_ctx:draw_screen_texture(textures[slot], rows[slot], columns[slot], temp_quad, custom_color);
                 --render_ctx:draw_screen_texture(textures[slot], sources[slot], temp_quad2, custom_color);
-
-            else 
-                --print("hat")
-                --prinspect(slot)
             end
         end
     end
@@ -150,11 +140,7 @@ set_callback(function (render_ctx, hud)
     render_ctx = render_ctx --[[@as VanillaRenderContext]]
     hud = hud --[[@as Hud]]
 
-    --0.86 base
-    --1.15 up
-
     for slot, value in pairs(hud.data.inventory) do
-        --prinspect(player_backitem)
         if(value.enabled)then
             local player_opacity;
             if options.a_old then
@@ -171,7 +157,6 @@ set_callback(function (render_ctx, hud)
             local y = 0.120 + hud.y;
             local b = 0.1;
             local temp_aabb = AABB:new(x, y, x + b, y - (b * (16/9)));
-            --temp_aabb = get_hud_position(slot)
             local temp_quad = Quad:new(temp_aabb);
 
             if player_backitem[slot] then
@@ -182,14 +167,13 @@ set_callback(function (render_ctx, hud)
 end, ON.RENDER_PRE_HUD)
 
 register_option_bool("a_old", "Use old visuals", "Icons don't adjust to player fade", false);
-
 --[[ delta
 register_option_bool("c_custom", "custom position", "Be precise", true)
 register_option_float("d_just", "SEEEE", "", 0.320, 0, 0)
 register_option_float("e_left", "left", "", -0.977, -1, 1)
 ]]
 --[[
-register_option_float("d_left", "left", "", -0.9775, -1, 1)
+register_option_string("d_left", "left", "", "0.016")
 register_option_float("e_top", "top", "", 0.120, -1, 1)
 register_option_float("f_big", "big", "", 0.1, 0, 2)
 ]]
